1508e3735bf61bd49258531c8addeea6a293ff4c
Matthew Cheetham <matt@3sidedcube.com>
2015-11-13 10:04:56 +0000

    Project setup

---
+++ CodebaseKit/CodebaseKit.h
@@ -0,0 +1,19 @@
+//
+//  CodebaseKit.h
+//  CodebaseKit
+//
+//  Created by Matthew Cheetham on 13/11/2015.
+//  Copyright © 2015 Matt Cheetham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CodebaseKit.
+FOUNDATION_EXPORT double CodebaseKitVersionNumber;
+
+//! Project version string for CodebaseKit.
+FOUNDATION_EXPORT const unsigned char CodebaseKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CodebaseKit/PublicHeader.h>
+
+
